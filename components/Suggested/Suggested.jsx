5df8500e529634bd8391b405d63d22a9d8ea339f
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-11-21 23:54:20 +0000

    remove suggested if no suggestions

--- components/Suggested/Suggested.jsx
+++ components/Suggested/Suggested.jsx
@@ -23,70 +23,70 @@ const Suggested = ({ suggested, movies, closeReadMore }) => {
     (suggested) => suggested.backdrop_path !== null
   );
 
+  // !suggested.results.length > 0
+
   return (
     <section className={styles.container}>
-      <div className={styles.heading}>suggested</div>
+      {!suggested.results.length > 0 ? null : (
+        <div className={styles.heading}>suggested</div>
+      )}
       <div className={styles.outerContainer}>
-        {!suggested.results.length > 0 ? (
-          <div className={styles.message}>No suggestions yet</div>
-        ) : (
-          <div
-            className={styles.suggestions}
-            ref={sliderRef}
-            onScroll={getScrollPosition}
-            onMouseEnter={mouseEnterSlide}
-          >
-            {suggestedArr.map((suggestion) => {
-              return (
-                <article key={suggestion.id} className={styles.linkContainer}>
-                  <Link
-                    href={
-                      movies
-                        ? `/movies/${suggestion.id}/${suggestion.title.replace(
-                            /\s+/g,
-                            "-"
-                          )}`
-                        : `/series/${suggestion.id}/${suggestion.name.replace(
-                            /\s+/g,
-                            "-"
-                          )}`
-                    }
-                    rel="preload"
+        <div
+          className={styles.suggestions}
+          ref={sliderRef}
+          onScroll={getScrollPosition}
+          onMouseEnter={mouseEnterSlide}
+        >
+          {suggestedArr.map((suggestion) => {
+            return (
+              <article key={suggestion.id} className={styles.linkContainer}>
+                <Link
+                  href={
+                    movies
+                      ? `/movies/${suggestion.id}/${suggestion.title.replace(
+                          /\s+/g,
+                          "-"
+                        )}`
+                      : `/series/${suggestion.id}/${suggestion.name.replace(
+                          /\s+/g,
+                          "-"
+                        )}`
+                  }
+                  rel="preload"
+                >
+                  <a
+                    className={styles.suggestionContainer}
+                    onClick={() => {
+                      closeReadMore();
+                      setTimeout(() => {
+                        sliderRef.current.scrollLeft = 0;
+                      }, 3000);
+                    }}
+                    ref={cardRef}
                   >
-                    <a
-                      className={styles.suggestionContainer}
-                      onClick={() => {
-                        closeReadMore();
-                        setTimeout(() => {
-                          sliderRef.current.scrollLeft = 0;
-                        }, 3000);
-                      }}
-                      ref={cardRef}
-                    >
-                      <Image
-                        src={`${SUGGESTIONS_URL_IMAGE}${suggestion.backdrop_path}`}
-                        alt={`${suggestion.title} backdrop`}
-                        unoptimized={true}
-                        placeholder="blur"
-                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                          shimmer(240, 140)
-                        )}`}
-                        layout="fill"
-                        objectFit="cover"
-                        className={styles.suggestionCard}
-                      />
-                    </a>
-                  </Link>
-                  {movies ? (
-                    <div className={styles.name}>{suggestion.title}</div>
-                  ) : (
-                    <div className={styles.name}>{suggestion.name}</div>
-                  )}
-                </article>
-              );
-            })}
-          </div>
-        )}
+                    <Image
+                      src={`${SUGGESTIONS_URL_IMAGE}${suggestion.backdrop_path}`}
+                      alt={`${suggestion.title} backdrop`}
+                      unoptimized={true}
+                      placeholder="blur"
+                      blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                        shimmer(240, 140)
+                      )}`}
+                      layout="fill"
+                      objectFit="cover"
+                      className={styles.suggestionCard}
+                    />
+                  </a>
+                </Link>
+                {movies ? (
+                  <div className={styles.name}>{suggestion.title}</div>
+                ) : (
+                  <div className={styles.name}>{suggestion.name}</div>
+                )}
+              </article>
+            );
+          })}
+        </div>
         {suggested.results.length === 0 || isScrollAtStart ? null : (
           <button
             className={`${styles.navigation} ${styles.navigationPrev}`}
