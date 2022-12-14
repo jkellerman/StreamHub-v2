375c1427084bdaac95f936b34e74acbcf33c40d3
Francisco Silva <fmsilva1996@gmail.com>
2022-12-14 16:07:20 +0000

    refactor: handle query params in useInfiniteScroll hook

--- hooks/useInfiniteScroll.js
+++ hooks/useInfiniteScroll.js
@@ -1,3 +1,4 @@
+import QueryString from "qs";
 import { useState, useEffect, useRef } from "react";
 
 const useInfiniteScroll = (endpoint) => {
@@ -5,25 +6,23 @@ const useInfiniteScroll = (endpoint) => {
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [newImages, setNewImages] = useState(false);
-  const mounted = useRef(false);
-  const url = `${endpoint}/${page}`;
 
   // fetch films/series everytime page in url changes
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        if (!url) return;
-        const response = await fetch(url);
+        const queryString = QueryString.stringify(
+          { page },
+          { addQueryPrefix: true }
+        );
+        const response = await fetch(`${endpoint}${queryString}`);
         const data = await response.json();
         const filteredArr = data.data.results.filter(
           (item) => item.backdrop_path !== null && !item.known_for_department
         );
         const arr = filteredArr;
 
-        setCards((prev) => {
-          if (page === 1) return arr;
-          else return [...prev, ...arr];
-        });
+        setCards((prev) => [...prev, ...arr]);
         setNewImages(false);
         setIsLoading(false);
       } catch (error) {
@@ -32,14 +31,9 @@ const useInfiniteScroll = (endpoint) => {
       }
     };
     fetchMovies();
-  }, [url]);
+  }, [endpoint, page]);
 
   useEffect(() => {
-    // Using mounted ref to stop newImages being rendered on initial render. Here we want new images only to render on 2nd, 3rd, 4th, etc.
-    if (!mounted.current) {
-      mounted.current = true;
-      return;
-    }
     if (!newImages) return;
     setPage((OldPage) => {
       return OldPage + 1;
