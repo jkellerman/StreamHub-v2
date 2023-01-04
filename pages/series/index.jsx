29b9280b4cf99d79301dac4af6dbd9d8006b4447
Francisco Silva <fmsilva1996@gmail.com>
2023-01-04 22:05:17 +0000

    refactor: index series page to handle genre pages

--- pages/series/index.jsx
+++ pages/series/index.jsx
@@ -1,8 +1,21 @@
 import Head from "next/head";
+import QueryString from "qs";
+import { useRouter } from "next/router";
+
+import Dropdown from "@/components/Dropdown/Dropdown";
 import SearchBar from "@/components/SearchBar/SearchBar";
-import MediaType from "@/components/MediaType/MediaType";
+import CardList from "@/components/CardList/CardList";
+
+import { DEFAULT_SERIES_GENRE } from "@/constants/app";
+import { BASE_TMDB_QUERY_PARAMS, BASE_TMDB_URL } from "@/constants/tmdb";
 
 const Series = ({ genreList }) => {
+  const { query, pathname } = useRouter();
+  const genre = genreList.find(({ name }) => name.toLowerCase() === query.genre) || DEFAULT_SERIES_GENRE;
+  const isDefaultGenre = genre.name === DEFAULT_SERIES_GENRE.name;
+  const endpoint = !isDefaultGenre ? `api/series/genre/${genre.id}` : "api/series/popular"
+  const pageType = pathname.replace(/\//g, '')
+
   return (
     <>
       <Head>
@@ -14,12 +27,17 @@ const Series = ({ genreList }) => {
       </Head>
       <main>
         <SearchBar series />
-        <MediaType
-          type="series"
-          endpoint="api/series/popular"
-          popular
-          seriesGenreList={genreList}
-        />
+        <section>
+          <Dropdown 
+            type={pageType}
+            selectedGenre={genre}
+            genreList={genreList}
+          />
+          <h1>{pageType}</h1>
+          <CardList
+            endpoint={endpoint}
+          />
+        </section>
       </main>
     </>
   );
@@ -29,13 +47,16 @@ export default Series;
 
 export async function getStaticProps() {
   const response = await fetch(
-    `https://api.themoviedb.org/3/genre/tv/list?api_key=${process.env.API_KEY}&language=en-GB`
+    `${BASE_TMDB_URL}/genre/tv/list?${QueryString.stringify(BASE_TMDB_QUERY_PARAMS)}`
   );
   const genreList = await response.json();
 
   return {
     props: {
-      genreList,
+      genreList: [
+        DEFAULT_SERIES_GENRE,
+        ...genreList.genres
+      ],
     },
   };
 }
