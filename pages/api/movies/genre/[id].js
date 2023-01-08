4d250cdecaf89e3b600096ca86d63a2351e00a5e
Francisco Silva <fmsilva1996@gmail.com>
2023-01-08 09:35:17 +0000

    chore: remove unused files and logs

--- pages/api/movies/genre/[id].js
+++ pages/api/movies/genre/[id].js
@@ -18,7 +18,6 @@ export default async function handler(req, res) {
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log("🚀 ~ file: [id].js:21 ~ handler ~ data", data);
 
     res.status(200).json({ data });
   } catch (error) {
