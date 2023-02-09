39dff9c4afac53d01979318198ffc3e817e2301a
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-02-09 11:44:15 +0000

    fix: incorrect dynamic routes for search

--- pages/search/all/[id].jsx
+++ pages/search/all/[id].jsx
@@ -6,15 +6,15 @@ import useInfiniteScroll from "hooks/useInfiniteScroll";
 
 const Search = () => {
   const {
-    query: { query },
+    query: { id },
   } = useRouter();
-  const endpoint = `/api/search/all/${query}`;
+  const endpoint = `/api/search/all/${id}`;
   const { cards, isLoading } = useInfiniteScroll(endpoint);
   return (
     <>
       <Head>
-        <title>{`${query} | Reelgood`}</title>
-        <meta name="description" content={`Where to watch ${query}`} />
+        <title>{`${id} | Reelgood`}</title>
+        <meta name="description" content={`Where to watch ${id}`} />
       </Head>
       <main>
         <SearchBar all />
@@ -22,8 +22,8 @@ const Search = () => {
           {!isLoading && (
             <h1>
               {cards.length !== 0
-                ? `Results found for '${query.replace(/-/g, " ")}'`
-                : `No Results found for '${query.replace(/-/g, " ")}'`}
+                ? `Results found for '${id.replace(/-/g, " ")}'`
+                : `No Results found for '${id.replace(/-/g, " ")}'`}
             </h1>
           )}
           <CardList cards={cards} isLoading={isLoading} />
