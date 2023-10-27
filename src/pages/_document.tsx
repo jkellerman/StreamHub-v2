3d758e60182082816a044e74c416795fc5c598ab
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-10-27 14:18:38 +0100

    chore: switch to local fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,20 +24,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;500;600&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Wix+Madefor+Display:wght@400;500;800&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Anton&display=swap"
-            rel="stylesheet"
-          />
+
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
