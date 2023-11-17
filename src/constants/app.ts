c1897fce54c78d5d743648eb6a052b6e7052358f
jkellerman <89940123+jkellerman@users.noreply.github.com>
2023-11-17 20:24:29 +0000

    chore: remove unnecessary constant

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,8 +1,7 @@
 import { Media } from "@/src/types";
 
-export const defaultGenreName = "Popular";
+export const defaultGenreName = "All Genres";
 export const defaultNetworkName = "All Services";
-export const defaultWatchGenreName = "All";
 export const defaultWatchNetworkName = "Any Service";
 
 export const DEFAULT_GENRE: Media.IGenre = {
@@ -13,10 +12,6 @@ export const DEFAULT_GENRE: Media.IGenre = {
 export const DEFAULT_NETWORK: Media.IServices = {
   provider_id: 1,
   provider_name: defaultNetworkName,
-};
-export const DEFAULT_WATCH_GENRE: Media.IGenre = {
-  id: 1,
-  name: defaultWatchGenreName,
 };
 
 export const DEFAULT_WATCH_NETWORK: Media.IServices = {
