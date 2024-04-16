44389725a876aa871227921e0585d48f678e0887
jkellerman <89940123+jkellerman@users.noreply.github.com>
2024-04-16 12:53:50 +0100

    refactor: update progressbar package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Router from "next/router";
-import NProgress from "nprogress";
+import NextTopLoader from "nextjs-toploader";
 import React from "react";
-
 import "@/styles/globals.scss";
 
 import { RegionProvider } from "../context/regionContext";
@@ -13,17 +11,13 @@ import type { AppProps } from "next/app";
 
 const queryClient = new QueryClient();
 
-NProgress.configure({ showSpinner: false }); // Page loader, progress bar from nprogress
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <RegionProvider>
           <CoreLayout>
+            <NextTopLoader color="var(--primary-light)" height={2} />
             <Component {...pageProps} />
           </CoreLayout>
           <ReactQueryDevtools />
